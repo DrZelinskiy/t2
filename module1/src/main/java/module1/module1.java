0e91d2f5013955fea833c319367ea651d9b6d378
Zalivko Aleksandr <zalivkoaleksandr@gmail.com>
2021-11-23 16:27:47 +0300

    Init multimodule like project

---
+++ module1/src/main/java/module1/module1.java
@@ -0,0 +1,44 @@
+package module1;
+
+// import java.util.Scanner;
+
+public class module1
+{
+    public static void main(String args[]){
+//      Scanner sc = new Scanner(System.in);
+        int i = 0;//, t = 0;
+//      loop: while (true) {
+        System.out.println("Start the count!");
+//      if (sc.hasNext()) {
+        for(i=1; i>=1; i++){
+            System.out.println("Runtime count: "+i);
+//                if (i == t) {
+//                  break;
+            try {
+                Thread.sleep(1000);
+            } 
+            catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }  
+                     
+
+//        t = sc.nextInt();
+//        if (t <= 0) {
+//            System.out.println("Target time value less then zero! Enter new value!");
+//            continue loop;
+            
+//        }
+//   }
+         
+//    }    
+    //System.out.println("Total runtime is "+i+" sec.");
+//    sc.close();
+//    break;         
+}    
+}
+//}
+
+//Добавлена линия для дополнительного пуша для тестирования работы пре пуш скрипта гит.
+//Вторая попытка. Сделал файл исполняемым.
+//Попытка три. Скрипт ушел в пре коммит действие.
