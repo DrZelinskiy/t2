a53195079491f387d19b4ec8fbe622f48ad3b370
Zalivko Aleksandr <zalivkoaleksandr@gmail.com>
2021-11-24 09:40:04 +0300

    добавил четвертый модуль для тестирования другого типа подъема версии пом файла

---
+++ module4/src/main/java/module4/module4.java
@@ -0,0 +1,44 @@
+package module4;
+
+// import java.util.Scanner;
+
+public class module4 
+{
+    public static void main(String args[]){
+//      Scanner sc = new Scanner(System.in);
+        int i = 0;//, t = 0;
+//      loop: while (true) {
+        System.out.println("Start the count!");
+//      if (sc.hasNext()) {
+        for(i=1; i>=1; i++){
+            System.out.println("Runtime count: "+i);
+//                if (i == t) {
+//                  break;
+            try {
+                Thread.sleep(1000);
+            } 
+            catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }  
+                     
+
+//        t = sc.nextInt();
+//        if (t <= 0) {
+//            System.out.println("Target time value less then zero! Enter new value!");
+//            continue loop;
+            
+//        }
+//   }
+         
+//    }    
+    //System.out.println("Total runtime is "+i+" sec.");
+//    sc.close();
+//    break;         
+}    
+}
+//}
+
+//Строка для изменение на третий модуль для проверка скрипта
+//Для третьего теста хука
+//Поспешишь – людей насмешишь
